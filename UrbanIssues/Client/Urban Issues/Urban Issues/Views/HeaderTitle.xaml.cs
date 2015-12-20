ec3301d59a4f7cfd252ff3ea058c556e967cb0a5
g-yonchev <georgi.d.yonchev@gmail.com>
2015-12-19 23:10:27 -0800

    Some kind of progress

--- UrbanIssues/Client/Urban Issues/Urban Issues/Views/HeaderTitle.xaml.cs
+++ UrbanIssues/Client/Urban Issues/Urban Issues/Views/HeaderTitle.xaml.cs
@@ -13,16 +13,13 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace UrbanIssues.Views
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
-	public sealed partial class BottomPage : Page
+	public sealed partial class HeaderTitle : UserControl
 	{
-		public BottomPage()
+		public HeaderTitle()
 		{
 			this.InitializeComponent();
 		}
